3ddc00723e83fa65ce70dfcf2a4cd3a014ec9c73
samupapati <samuelpapati@gmail.com>
2023-04-21 22:54:33 -0300

    adicionando limitação de numero de paginas

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,20 @@
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai"
 
-function Buttons({handlePage}){
+function Buttons({handlePage, counter, nPages}){
     return (
         <>
-            <button className="btn-pass" id="btn-pass-left"  onClick={() => handlePage('left')}><AiFillCaretLeft className="btn-pass-icon"/></button>
-            <button className="btn-pass" id="btn-pass-right" onClick={() => handlePage('right')}><AiFillCaretRight className="btn-pass-icon"/></button>
+            {counter === 1 
+            ? 
+                ''
+            :
+                <button className="btn-pass" id="btn-pass-left"  onClick={() => handlePage('left')}><AiFillCaretLeft className="btn-pass-icon"/></button>
+            }
+            {counter === nPages 
+            ? 
+                ''
+            :
+                <button className="btn-pass" id="btn-pass-right" onClick={() => handlePage('right')}><AiFillCaretRight className="btn-pass-icon"/></button>
+            }
         </>
     )
 }
